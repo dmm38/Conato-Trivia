dab0364ce2226aedd049e6d638e8106784376661
dmm38 <47959145+dmm38@users.noreply.github.com>
2019-03-07 22:30:50 -0600

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,63 @@
+    // Object for storing quiz questions
+    var questions = [
+        {
+            question : 'Al activarse la alarma de incendio la brigada debe',
+            answers : ['Evacuar hacia un punto de reuni\xf3n. ', 'SRecargar los extintores.  ', 'Desplazarse a la zona del evento. ','Todas las anteriores son falsas.  '],
+            correctAnswer : 2, // questionIndex of correct anwers
+            imageId : '1' // ID for triggering image change
+        },
+        {
+            question : 'Al activarse la alarma de incendio el personal que no forma parte de la brigada debe:',
+            answers : ['Evacuar hacia un punto de reuni\xf3n. ', 'Recargar los extintores.  ', 'Desplazarse a la zona del evento.  ', 'Todas las anteriores son falsas.'],
+            correctAnswer : 0, 
+            imageId : '2'
+        },
+        {
+            question : 'Qui\xE9n debe llamar a la estaci\xf3n de bomberos? ',
+            answers : ['Personal administrativo ', 'La primera persona que visualice el evento  ', 'El coordinador de la brigada  ', 'Cualquier persona en la brigada. '],
+            correctAnswer : 0, 
+            imageId : '5'
+        },
+        {
+            question : 'Despu\xE9s de que se controla una emergencia la brigada debe:  ',
+            answers : ['Recargar los extintores.  ', 'Presentar un informe al comit\xE9 de emergencia.  ', 'Realizar una autoevaluaci\xf3n.  ', 'Todas son ciertas '],
+            correctAnswer : 3,
+            imageId : '6'
+        },
+        {
+            question : 'Qui\xE9n es responsable de recargar los extintores utilizados?',
+            answers : ['Personal administrativo  ', 'El comit\xE9 de emergencias  ', 'El coordinador de la brigada  ', 'Cualquier persona en la brigada.  '],
+            correctAnswer : 2, 
+            imageId : '10'
+        },
+        {
+            question : 'En caso de incendio, se debe llamar siempre a la estaci\xf3n de bomberos?',
+            answers : ['No, solo se llama si el incendio  se sale de control ', 'Sí, por menor que sea el incendio debe llamarse a los bomberos.  ', 'Depende del personal administrativo.  ', 'Depende del criterio del coordinador de la brigada. '],
+            correctAnswer : 1, 
+            imageId : '12'
+        },
+        {
+            question : 'En caso de  incendio, Qui\xE9n activa la alarma?',
+            answers : ['Personal administrativo  ', 'La primera persona que visualice el evento  ', 'El coordinador de la brigada   ', 'Cualquier persona en la brigada. '],
+            correctAnswer : 1, 
+            imageId : '13'
+        },
+        {
+            question : 'Despu\xE9s de llamar a los bomberos se debe:',
+            answers : ['Revisar el estado de los extintores', 'Desplazarse al lugar del evento  ', 'Identificar el tipo de incendio ', 'Intentar controlar el incendio con el equipo respectivo '],
+            correctAnswer : 3, 
+            imageId : '14'
+        },
+        {
+            question : 'Despu\xE9s de identificar la gravedad del evento se debe:',
+            answers : ['Llamar a la estaci\xf3n de los bomberos', 'Intentar controlar el incendio con el equipo respectivo ', 'Activar la alarma ', 'Realizar una autoevaluaci\xf3n '],
+            correctAnswer : 0, 
+            imageId : '4'
+        },
+        {
+            question : 'Durante la emergencia es importante: ',
+            answers : ['Nunca abrir una puerta sin antes palparla', 'Cerrar portones/puertas con seguro al salir ', 'Tratar de tapar boca y nariz con un pa\xF1o h\xFAmedo ', 'A y C son correctas '],
+            correctAnswer : 3, 
+            imageId : '11'
+        }
+    ];
